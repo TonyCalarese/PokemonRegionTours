d624cdc87f4d5485a5c57615e18096df246c6b5c
Tony Calarese <accalarese@gmail.com>
2020-04-02 13:29:32 -0400

    image picker goes to files but does not insert picture

--- PokemonRegionTours/ViewControllers/Notes/NotesDetailViewController.swift
+++ PokemonRegionTours/ViewControllers/Notes/NotesDetailViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class NotesDetailViewController: UIViewController {
+class NotesDetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
     var notes: Notes? = nil
-    var imageString: String = "Eevee"
     
+    //source of image picker: https://stackoverflow.com/questions/25510081/how-to-allow-user-to-pick-the-image-with-swift
+    var imageString: String = "Eevee"
+    var imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var ImageButton: UIButton!
     @IBOutlet weak var NoteTitle: UITextField!
     @IBOutlet weak var NotesImage: UIImageView!
     @IBOutlet weak var BodyText: UITextView!
@@ -24,8 +28,28 @@ class NotesDetailViewController: UIViewController {
             sender.text = "New Note"
         }
     }
-    
-    
+    /* begin of source for image picker */
+    @IBAction func uploadImage(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+                   print("Button capture")
+
+                   imagePicker.delegate = self
+                   imagePicker.sourceType = .savedPhotosAlbum
+                   imagePicker.allowsEditing = false
+
+                   present(imagePicker, animated: true, completion: nil)
+               }
+        
+        func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!){
+            self.dismiss(animated: true, completion: { () -> Void in
+
+            })
+
+            NotesImage.image = image
+        }
+        
+    }
+    // of image picker source
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,8 +72,8 @@ class NotesDetailViewController: UIViewController {
             notes?.desc = body
         }
         if let image = NotesImage.image {
-            UIImage(contentsOfFile: String)
-            notes?.image = image
+      //      UIImage(contentsOfFile: String)
+      //      notes?.image = image
         }
     }
 
