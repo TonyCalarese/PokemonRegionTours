f1e6656ea0934d29c0af53947aee527b994833ea
Tony Calarese <accalarese@gmail.com>
2020-04-01 22:05:07 -0400

    everything working but the image going to take another approach

--- PokemonRegionTours/ViewControllers/Notes/NotesDetailViewController.swift
+++ PokemonRegionTours/ViewControllers/Notes/NotesDetailViewController.swift
@@ -9,24 +9,48 @@
 import UIKit
 
 class NotesDetailViewController: UIViewController {
-
-    @IBOutlet weak var NoteTitle: UITextField!
-    @IBOutlet weak var NoteImage: UIImageView!
-    @IBOutlet weak var BodyText: UITextView!
     var notes: Notes? = nil
+    var imageString: String = "Eevee"
+    
+    @IBOutlet weak var NoteTitle: UITextField!
+    @IBOutlet weak var NotesImage: UIImageView!
+    @IBOutlet weak var BodyText: UITextView!
+    
+    @IBAction func UpdateNotesTitle(_ sender: UITextField) {
+        if let title = sender.text {
+            notes?.name = title
+        }
+        else{
+            sender.text = "New Note"
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         //Set the Default data
         if let userNotes = notes{
             NoteTitle.text = userNotes.name
             if let img = userNotes.image {
-                NoteImage.image = UIImage(named: img)
+                NotesImage.image = UIImage(named: img)
             }
         
             BodyText.text = userNotes.desc
         }
         
         
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        if let body = BodyText.text {
+            notes?.desc = body
+        }
+        if let image = NotesImage.image {
+            UIImage(contentsOfFile: String)
+            notes?.image = image
+        }
     }
 
 
